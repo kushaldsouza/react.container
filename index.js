52a60757fd450576856c302ffa4f4864d5ed0825
Colonna, Olivier <olivier.colonna@accenture.com>
2016-01-10 20:23:49 +0000

    defaultProps setup

--- index.js
+++ index.js
@@ -2,13 +2,28 @@ import React from 'react';
 
 class Container extends React.Component {
 
-  constructor () {
+  constructor (props) {
+    super(props);
+  }
 
+  render () {
+    return {
+      <div></div>
+    };
   }
 
 }
 
+Container.defaultProps = {
+  height: null,
+  width: null,
+  hidden: false,
+  id: void 0,
+  cls: null,
+  padding: null,
+  style: null
+};
+
 Container.propTypes = {};
-Container.defaultProps = {};
 
 export default Container;
