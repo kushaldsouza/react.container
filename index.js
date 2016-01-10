6237844db9ba4088616f6bb96f198be03e8725b1
Colonna, Olivier <olivier.colonna@accenture.com>
2016-01-10 19:34:18 +0000

    Entry point of React-Container

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Container extends React.Component {
+
+  constructor () {
+
+  }
+
+}
+
+Container.propTypes = {};
+Container.defaultProps = {};
+
+export default react-container;
