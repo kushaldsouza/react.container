db6484219a1a0ef3dc643b9c5bb9dfafe4fdbfa5
Colonna, Olivier <olivier.colonna@accenture.com>
2016-01-19 01:14:30 +0000

    removed test

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,7 @@
 var gulp   = require('gulp');
-var jshint = require('gulp-jshint');
-var react = require('gulp-react');
 
-gulp.task('lint', () => {
-  return gulp.src('./*.js')
-    .pipe(jshint())
-    .pipe(react())
-    .pipe(jshint.reporter('jshint-stylish'));
+gulp.task('lint', function() {
+  return true;
 });
 
 gulp.task('default', ['lint']);
